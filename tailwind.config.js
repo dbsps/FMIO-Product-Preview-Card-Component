e93d7a22a1b686f2034a8c20500645e7a6926e4b
Kris Roadruck <kris@roadruck.com>
2022-07-17 08:39:25 -0400

    colors & fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.{html,js}"],
   theme: {
+    fontFamily: {
+      'montserrat': ['montserrat', 'sans-serif'],
+      'fraunces': ['fraunces', 'serif']
+    },
+    colors: {
+      'white': 'hsl(0, 0%, 100%)',
+      'cream': 'hsl(30, 38%, 92%)',
+      'aurometal': 'hsl(228, 12%, 48%)',
+      'gunmetal': 'hsl(212, 21%, 14%)',
+      'aquamarine': 'hsl(158, 36%, 37%)',
+      'dark-aquamarine': 'hsl(158, 42%, 18%)'
+      },
     extend: {},
   },
   plugins: [],
